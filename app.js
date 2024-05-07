3271b2590c22f51fe5644111ccf601b252df07ad
KAlexRussel <kouawoualex1234@gmail.com>
2024-05-07 08:43:49 +0100

    connected succesfully to database mongodb
    Signed-off-by: KAlexRussel <kouawoualex1234@gmail.com>

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 // import express from "express"; es module
-const app = express();
-
+const mongoose = require("mongoose");
 require("dotenv").config();
 
+// app
+const app = express();
+
+// db
+mongoose.connect(process.env.Database).then(() => console.log("DB connect"));
+
 app.get("/", (req, res) => {
-  res.send("hello from node");
+  res.send("hello from node update");
 });
 
 const port = process.env.PORT || 8000;
